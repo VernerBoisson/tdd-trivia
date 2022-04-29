0b2a77fea62abfa0773703ea86565992768a3d61
VernerBoisson <verner.boisson@gmail.com>
2022-04-29 19:41:16 +0200

    _current_category duplicate remove

--- trivia.py
+++ trivia.py
@@ -82,15 +82,9 @@ class Game:
 
     @property
     def _current_category(self):
-        if self.places[self.current_player] == 0: return 'Pop'
-        if self.places[self.current_player] == 4: return 'Pop'
-        if self.places[self.current_player] == 8: return 'Pop'
-        if self.places[self.current_player] == 1: return 'Science'
-        if self.places[self.current_player] == 5: return 'Science'
-        if self.places[self.current_player] == 9: return 'Science'
-        if self.places[self.current_player] == 2: return 'Sports'
-        if self.places[self.current_player] == 6: return 'Sports'
-        if self.places[self.current_player] == 10: return 'Sports'
+        if self.places[self.current_player] in [0,4,8]: return 'Pop'
+        if self.places[self.current_player] in [1,5,9]: return 'Science'
+        if self.places[self.current_player] in [2,6,10]: return 'Sports'
         return 'Rock'
 
     def was_correctly_answered(self):
