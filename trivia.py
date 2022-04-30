a4d74abf5a2a6a23d59643c5ea1367d805776227
VernerBoisson <verner.boisson@gmail.com>
2022-04-30 23:34:22 +0200

    file constant

--- trivia.py
+++ trivia.py
@@ -2,36 +2,20 @@
 from random import randint
 from utils import Utils
 from questions import Questions
-from players import Players, Player
-
-CONSTANT = {
-    'NUMBER_OF_QUESTIONS_BY_CATEGORY': 50,
-    'LIST_OF_CATEGORIES': ['Pop', 'Science', 'Sports', 'Rock'],
-    'MIN_NUMBER_OF_PLAYERS': 2,
-    'POINT_FOR_WINNING': 6,
-    'DICE_FACE': 6,
-    'NUMBER_OF_PLACES': 12,
-}
-
-
-dict_places_category = {
-    (0,4,8): CONSTANT['LIST_OF_CATEGORIES'][0],
-    (1,5,9): CONSTANT['LIST_OF_CATEGORIES'][1],
-    (2,6,10): CONSTANT['LIST_OF_CATEGORIES'][2],
-    (3,7,11): CONSTANT['LIST_OF_CATEGORIES'][3],
-}
+from players import Players
+import config_game as ConfigGame
 
 class Game:
     def __init__(self):
         self.players = Players()
         self.current_player = 0
         self.list_all_questions = {}
-        self._init_list_categories(CONSTANT['LIST_OF_CATEGORIES'])
+        self._init_list_categories(ConfigGame.LIST_OF_CATEGORIES)
 
     def _init_list_categories(self, list_of_categories):
         for category in list_of_categories:
             self.list_all_questions[category] = Questions(category)
-            self.list_all_questions[category].generate_questions(CONSTANT['NUMBER_OF_QUESTIONS_BY_CATEGORY'])
+            self.list_all_questions[category].generate_questions(ConfigGame.NUMBER_OF_QUESTIONS_BY_CATEGORY)
 
     def _get_current_player(self):
         return self.players.get_player_by_index(self.current_player)
@@ -40,7 +24,7 @@ class Game:
         return self._get_current_player().name
 
     def _get_question_category(self):
-        return Utils.multi_key_dict_get(dict_places_category, self._get_current_player().place)
+        return Utils.multi_key_dict_get(ConfigGame.dict_places_category, self._get_current_player().place)
     
     def start(self):
         while self._is_playable():
@@ -54,7 +38,7 @@ class Game:
             self._change_current_player()        
 
     def _roll_dice(self):
-        return randint(1, CONSTANT['DICE_FACE'])
+        return randint(1, ConfigGame.DICE_FACE)
 
     def add_players(self, list_of_players):
         self.players.add_players(list_of_players)
@@ -68,8 +52,8 @@ class Game:
 
     def _move_player_place(self, roll_dice):
         self._get_current_player().place += roll_dice
-        if self._get_current_player().place >= CONSTANT['NUMBER_OF_PLACES']:
-            self._get_current_player().place -= CONSTANT['NUMBER_OF_PLACES']
+        if self._get_current_player().place >= ConfigGame.NUMBER_OF_PLACES:
+            self._get_current_player().place -= ConfigGame.NUMBER_OF_PLACES
         self._log_move_player()
        
     def _log_move_player(self):
@@ -122,10 +106,10 @@ class Game:
         if self.current_player > self.players.last_index(): self.current_player = 0
 
     def _is_current_player_winner(self):
-        return self._get_current_player().score >= CONSTANT['POINT_FOR_WINNING']
+        return self._get_current_player().score >= ConfigGame.POINT_FOR_WINNING
 
     def _is_playable(self):
-        return self.players.__len__() >= CONSTANT['MIN_NUMBER_OF_PLAYERS']
+        return self.players.__len__() >= ConfigGame.MIN_NUMBER_OF_PLAYERS
 
 if __name__ == '__main__':
     game = Game()
